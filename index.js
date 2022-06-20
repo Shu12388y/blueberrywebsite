72b6db803f66f49e21c1aa57aa7945460f15f8f3
Shu12388y <91714143+Shu12388y@users.noreply.github.com>
2022-06-20 19:15:50 +0530

    upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("hello friends , welcome to our fruit website");
+    let name=prompt("write your name");
+   
+   
+    
+   
+if(name=="shubham paul"){
+    alert("Hello Admin")
+}
+else{
+    alert("welcome to our website 'Blue Berrys' " + name);
+}
+console.log(name);
